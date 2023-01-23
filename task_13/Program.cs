62d6b1d784040328b2fa7e2c1e91b8af1a3030ac
Semen <1488semen@mail.ru>
2023-01-23 23:47:53 +0300

    Решение

--- task_13/Program.cs
+++ task_13/Program.cs
@@ -1,37 +1,23 @@
-﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-456 -> 5
-782 -> 8
-918 -> 1
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
 */
+       
+Console.WriteLine("Введите первое число");     
+string str = Console.ReadLine();
+ int amount = str.ToString().Length;
 
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        int number = ReadInt("Введите трехзначное число: ");
-        int amount = number.ToString().Length;
-
-        if (amount < 3 || amount > 3)
+if (amount < 3 )
         {
-            Console.WriteLine("Вы ввели не трехзначное число");
+            Console.WriteLine("третьей цифры нет");
         }
         else
-        {
-            Console.WriteLine(InCenter(number));
-        }
-
-        int ReadInt(string message)
-        {
-            Console.Write(message);
-            return Convert.ToInt32(Console.ReadLine());
-        }
-
-        int InCenter(int a)
-        {
-
-            int result = a / 10 % 10;
-            return result;
-        }
-    }
+{
+  Console.WriteLine(str[2]);  
 }
+
+
